deb8e652ab387035e98c184a62afc709640e50fa
Ryo Rafael <ryorafael18@gmail.com>
2024-02-26 15:11:46 +0100

    portfolio react.js
    using react.js for the first time to make my portfolio

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src="" alt="" />
+      <div className="desktopMenu"></div>
+      <button className="desktopMenuBtn">
+        <img src="" alt="" className="desktopMenuImg" /> Contact Me
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
