5efa3de26b34d52fbc621ee75813d4058904a413
Ryo Rafael <ryorafael18@gmail.com>
2024-03-06 17:25:18 +0100

    added resume

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./intro.css";
 import { Link as ScrollLink } from "react-scroll";
 import { GrContact } from "react-icons/gr";
+import myPdf from "../../assets/myPdfFile.pdf";
 
 const Intro = () => {
+  const openPDF = () => {
+    window.open(myPdf, "_blank");
+  };
   return (
     <section id="intro" className="intro">
       <div className="introContent">
@@ -18,7 +22,7 @@ const Intro = () => {
           2024 for one year, 4 days a week.
         </p>
         <ScrollLink to="about" smooth={true} duration={500} className="btn">
-          <button className="btn">
+          <button className="btn" onClick={openPDF}>
             <GrContact className="btnIcon" />
           </button>
         </ScrollLink>
