25ee8ecab666ef213948ded2a6198d127a3ccb63
Ryo Rafael <ryorafael18@gmail.com>
2024-03-18 16:10:52 +0100

    background changed

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer/footer";
+import backgroundImage from './assets/bg.png';
 
 function App() {
+  const backgroundColor = '#291c3a'; 
+
+  const appStyle = {
+    position: 'relative', 
+    backgroundImage: `url(${backgroundImage})`, // Use background image
+    backgroundColor: backgroundColor, // Use background color if no image
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundAttachment: 'fixed', // Fixed background
+    minHeight: '100vh', // Ensure the container takes up at least the height of the viewport
+    // Add other styles as needed
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={appStyle}>
       <Navbar />
       <Intro />
       <Skills />
