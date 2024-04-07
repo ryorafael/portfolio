d86974337588481abd05dd0b399c9bacd4ac8d09
Ryo Rafael <ryorafael18@gmail.com>
2024-04-07 14:43:51 +0200

    update cv

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./intro.css";
 import { FaDownload } from "react-icons/fa6";
-import myPdf from "../../assets/myPdfFile.pdf";
-import hazeImg from "../../assets/hazedrawing.png"
+import myPdf from "../../assets/ryorafaelcv.pdf";
+import hazeImg from "../../assets/hazedrawing.png";
 import { useTranslation } from "react-i18next";
 
 const Intro = () => {
-  const { t } = useTranslation();  
+  const { t } = useTranslation();
   const openPDF = () => {
     window.open(myPdf, "_blank");
   };
@@ -16,20 +16,19 @@ const Intro = () => {
         <span className="hello">{t("Hello")},</span>
         <span className="introText">
           {t("I'm")}
-          <span className="introName">
-            {" "}
-            Ryo Rafael
-          </span>
+          <span className="introName"> Ryo Rafael</span>
           <br />
           {t("Fullstack Developer")}
         </span>
         <p className="introPara">
           {t("I am looking for an internship starting from September 2024.")}
         </p>
-          <button className="btn" onClick={openPDF}>
-            <FaDownload className="btnIcon" />
-            <span className="btnText">{t("Downloadable version of my resume")}</span>
-          </button>
+        <button className="btn" onClick={openPDF}>
+          <FaDownload className="btnIcon" />
+          <span className="btnText">
+            {t("Downloadable version of my resume")}
+          </span>
+        </button>
       </div>
       <img src={hazeImg} alt={t("illustration by xopolin")} className="bg" />
     </section>
